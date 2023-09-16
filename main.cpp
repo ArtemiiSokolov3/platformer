3b1a60ca9f107c48063c318809f2d21edde4e3e1
ArtemiiSokolov3 <113536426+ArtemiiSokolov3@users.noreply.github.com>
2023-09-16 17:53:42 +0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,9 @@
 int main()
 {
     txCreateWindow(800, 600);
-
+    txSetFillColor(TX_WHITE);
+    txClear();
+    txSetColor(TX_BLACK);
+    txSetFillColor(TX_BLACK);
+    txRectangle(100,100,350,150);
 }
