68876ea62f2199c004b5387c7b48c9791b03a326
ArtemiiSokolov3 <113536426+ArtemiiSokolov3@users.noreply.github.com>
2023-09-20 18:05:17 +0400

    Update Classes.cpp

--- Classes.cpp
+++ Classes.cpp
@@ -14,6 +14,22 @@ public:
     HDC right = txLoadImage("playerright.png");
     HDC left = txLoadImage("playerleft.png");
 
+    Player(int sizeX1, int sizeY1, int x1, int y1)
+    {
+        sizeX=sizeX1;
+        sizeY=sizeY1;
+        x=x1;
+        y=y1;
+    } 
+    Player(int sizeX1, int sizeY1, int x1, int y1, string fileL, string fileR)
+    {
+        sizeX=sizeX1;
+        sizeY=sizeY1;
+        x=x1;
+        y=y1;
+        right = txLoadImage(fileR);
+        left = txLoadImage(fileL);
+    } 
     void playerright_draw()
     {
         txTransparentBlt(txDC(), x, y, sizeX, sizeY, right);
@@ -27,21 +43,25 @@ public:
 
 };
 
-class platform
+class Platform
 {
 public:
     int sizeX = 200;
-    int sixeY = 75;
+    int sizeY = 75;
     HDC left = txLoadImage("platform.png");
-
+    Platform(int sizeX1, int sizeY1)
+    {
+        sizeX=sizeX1;
+        sizeY=sizeY1;
+    }
+    Platform(int sizeX1, int sizeY1, string fileK)
+    {
+        sizeX=sizeX1;
+        sizeY=sizeY1;
+        left = txLoadImage(fileK);
+    }
     void platform_draw(int x, int y)
     {
-        txTransparentBlt(txDC(), x, y, sizeX, sixeY, left);
+        txTransparentBlt(txDC(), x, y, sizeX, sizeY, left);
     }
 };
-int main()
-{
-    cout << "Hello World!";
-
-    return 0;
-}
