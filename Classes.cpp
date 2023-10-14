9efb617bad636d9b7b0bb53095b8def166f3a9cb
ArtemiiSokolov3 <113536426+ArtemiiSokolov3@users.noreply.github.com>
2023-10-14 18:08:08 +0400

    Update Classes.cpp

--- Classes.cpp
+++ Classes.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Player
 {
     public:
+    float dy = 0;
     int sizeX = 74;
     int sizeY = 81;
     int jump = 50;
@@ -11,21 +12,22 @@ class Player
     int x = 41;
     int y = 38;
     int arrow = 1;
+    int groundLevel = 500;
     HDC right = txLoadImage("playerright.bmp");
     HDC left = txLoadImage("playerleft.bmp");
     Player(int sizeX1, int sizeY1, int x1, int y1)
     {
-        sizeX=sizeX1;
-        sizeY=sizeY1;
-        x=x1;
-        y=y1;
+        sizeX = sizeX1;
+        sizeY = sizeY1;
+        x = x1;
+        y = y1;
     }
     Player(int sizeX1, int sizeY1, int x1, int y1, string fileL, string fileR)
     {
-        sizeX=sizeX1;
-        sizeY=sizeY1;
-        x=x1;
-        y=y1;
+        sizeX = sizeX1;
+        sizeY = sizeY1;
+        x = x1;
+        y = y1;
         right = txLoadImage(fileR.c_str());
         left = txLoadImage(fileL.c_str());
     }
@@ -44,55 +46,84 @@ class Player
     {
         if (txGetAsyncKeyState(VK_LEFT))
         {
-            if (x>0) {x=x-5; arrow=1; }
+            if (x>0) {x=x-speed; arrow=1;}
         }
         if (txGetAsyncKeyState(VK_RIGHT))
         {
-            if (x<750) {x=x+5; arrow=0;}
+            if (x<750) {x=x+speed; arrow=0;}
         }
         if (txGetAsyncKeyState(VK_UP))
         {
-            if (y>300) y=y-5;
+            if (y<300) y=y-speed-20;
         }
+    }
+    int Falling_Player(int level)
+    {
+
+        if (y < level)
+        {
+            dy += 0.5;
+            y += dy;
+        }
+    }
+    int OnPl (int pltformY, int platformX, int platformSizeX)
+    {
+        if (y < pltformY && x < platformX + platformSizeX && x > platformX)
+        return 1;
+        return 0;
     }
 };
-    class Platform
-    {
+class Platform
+{
     public:
-        int sizeX = 200;
-        int sizeY = 75;
-        int x = 40;
-        int y = 100;
-        HDC left = txLoadImage("platform.bmp");
-        Platform(int sizeX1, int sizeY1)
-        {
-            sizeX=sizeX1;
-            sizeY=sizeY1;
-        }
-        Platform(int sizeX1, int sizeY1, string fileK)
-        {
-            sizeX=sizeX1;
-            sizeY=sizeY1;
-            left = txLoadImage(fileK.c_str());
-        }
-        int platform_draw()
-        {
-            txTransparentBlt(txDC(), x, y, sizeX, sizeY, left,0,0, TX_WHITE);
-
-        }
-    };
-
-    int main()
+    int sizeX = 200;
+    int sizeY = 75;
+    int x = 40;
+    int y = 200;
+    HDC left = txLoadImage("platform.bmp");
+    Platform(int sizeX1, int sizeY1)
     {
-        txCreateWindow(800, 600);
-        Platform b(200, 75);
-        Player a(74, 81, 41, 38);
-        while(1)
-        {
-            a.playerDraw();
-            a.Move();
-            b.platform_draw();
-            txSleep(100);
-            txClear();
-        }
+        sizeX=sizeX1;
+        sizeY=sizeY1;
     }
+    Platform(int sizeX1, int sizeY1, string fileK)
+    {
+        sizeX=sizeX1;
+        sizeY=sizeY1;
+        left = txLoadImage(fileK.c_str());
+    }
+    int platform_draw()
+    {
+        txTransparentBlt(txDC(), x, y, sizeX, sizeY, left,0,0, TX_WHITE);
+    }
+};
+
+int main()
+{
+    txCreateWindow(800, 600);
+    Platform platform(200, 75);
+    Player player(74, 81, 41, 38);
+    while(1)
+    {
+        player.playerDraw();
+        player.Move();
+        platform.platform_draw();
+        /*
+        if (1)
+        {
+            player.fallY = platform.y - platform.sizeY;
+            player.fallX = platform.x + platform.sizeX;
+        }
+        */
+        if (player.OnPl(platform.y, platform.x, platform.sizeX))
+        {
+            player.Falling_Player(platform.y - platform.sizeY);
+        }
+        else
+        {
+            player.Falling_Player(player.groundLevel);
+        }
+        txSleep(100);
+        txClear();
+    }
+}
